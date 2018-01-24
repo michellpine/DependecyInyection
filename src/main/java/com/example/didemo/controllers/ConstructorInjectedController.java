c703fd444df17cbd872bbcd01d21c2c8c087181e
Michell <michellpine@hotmail.com>
2018-01-24 11:41:56 -0500

    Saludo en german english y español

--- src/main/java/com/example/didemo/controllers/ConstructorInjectedController.java
+++ src/main/java/com/example/didemo/controllers/ConstructorInjectedController.java
@@ -1,6 +1,7 @@
 package com.example.didemo.controllers;
 
 import com.example.didemo.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
@@ -12,6 +13,7 @@ public class ConstructorInjectedController {
     //Hago uso de la interfaz no de la propia clase
     private GreetingService greetingService;
 
+    @Autowired
     public ConstructorInjectedController(@Qualifier("constructorGreetingService") GreetingService greetingService) {
         this.greetingService = greetingService;
     }
--- src/main/java/com/example/didemo/controllers/MyController.java
+++ src/main/java/com/example/didemo/controllers/MyController.java
@@ -13,7 +13,7 @@ public class MyController {
     }
 
     public String hello(){
-        System.out.println("Hello!!!");
+        System.out.println("Hello!!!    ");
         return greetingService.sayGreeting();
     }
 }
