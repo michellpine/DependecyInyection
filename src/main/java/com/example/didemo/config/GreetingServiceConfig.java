f6eb8bef0961bcda578e80ac8219f95ae71a1d44
Michell <michellpine@hotmail.com>
2018-01-24 14:30:10 -0500

    Usando notacion configuracion

---
+++ src/main/java/com/example/didemo/config/GreetingServiceConfig.java
@@ -0,0 +1,41 @@
+package com.example.didemo.config;
+
+import com.example.didemo.repository.GreetingRepository;
+import com.example.didemo.services.GreetingService;
+import com.example.didemo.services.GreetingServiceFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+
+
+@Configuration
+public class GreetingServiceConfig {
+
+    @Bean
+    GreetingServiceFactory greetingServiceFactory(GreetingRepository repository){
+        return new GreetingServiceFactory(repository);
+    }
+
+    @Bean
+    @Primary
+    @Profile({"default", "en"})
+    GreetingService primaryGreetingService(GreetingServiceFactory greetingServiceFactory){
+        return greetingServiceFactory.createGreetingSevice("en");
+    }
+
+    @Bean
+    @Primary
+    @Profile("es")
+    GreetingService primarySpanishGreetingService(GreetingServiceFactory greetingServiceFactory){
+        return greetingServiceFactory.createGreetingSevice("es");
+    }
+
+    @Bean
+    @Primary
+    @Profile("de")
+    GreetingService primaryGermanGreetingService(GreetingServiceFactory greetingServiceFactory){
+        return greetingServiceFactory.createGreetingSevice("de");
+    }
+}
+
--- src/main/java/com/example/didemo/services/PrimaryGermanGreetingService.java
+++ src/main/java/com/example/didemo/services/PrimaryGermanGreetingService.java
@@ -1,13 +1,7 @@
 package com.example.didemo.services;
 
 import com.example.didemo.repository.GreetingRepository;
-import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.Profile;
-import org.springframework.stereotype.Service;
 
-@Service
-@Primary
-@Profile("de")
 public class PrimaryGermanGreetingService implements GreetingService {
 
     private GreetingRepository greetingRepository;
