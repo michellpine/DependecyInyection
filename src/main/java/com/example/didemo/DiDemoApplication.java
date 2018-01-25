d58ed0d14a98f5e656187e5e31df80feca8c8225
Michell <michellpine@hotmail.com>
2018-01-25 06:47:02 -0500

    Externalizando propiedades

--- src/main/java/com/example/didemo/DiDemoApplication.java
+++ src/main/java/com/example/didemo/DiDemoApplication.java
@@ -4,6 +4,8 @@ import com.example.didemo.controllers.ConstructorInjectedController;
 import com.example.didemo.controllers.MyController;
 import com.example.didemo.controllers.PropertyInjectedController;
 import com.example.didemo.controllers.SetterInjectedController;
+import com.example.didemo.examplebeans.FakeDataSource;
+import com.sun.org.apache.xpath.internal.SourceTree;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -15,9 +17,8 @@ public class DiDemoApplication {
 
 		MyController controller = (MyController) ctx.getBean("myController");
 
-		System.out.println( controller.hello());
-		System.out.println(ctx.getBean(PropertyInjectedController.class).sayHello());
-		System.out.println(ctx.getBean(SetterInjectedController.class).sayHello());
-		System.out.println(ctx.getBean(ConstructorInjectedController.class).sayHello());
+		FakeDataSource fakeDataSource = (FakeDataSource) ctx.getBean(FakeDataSource.class);
+		System.out.println(fakeDataSource.getUser());
+
 	}
 }
---
+++ src/main/java/com/example/didemo/config/PropertyConfig.java
@@ -0,0 +1,38 @@
+package com.example.didemo.config;
+
+import com.example.didemo.examplebeans.FakeDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+@Configuration
+@PropertySource("classpath:datasource.properties")
+public class PropertyConfig {
+
+    @Value("${demo.user}")
+    String user;
+
+    @Value("${demo.password}")
+    String password;
+
+    @Value("${demo.dburl}")
+    String url;
+
+    @Bean
+    public FakeDataSource fakeDataSource(){
+        FakeDataSource fakeDataSource = new FakeDataSource();
+        fakeDataSource.setUser(user);
+        fakeDataSource.setPassword(password);
+        fakeDataSource.setUrl(url);
+        return fakeDataSource;
+    }
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer properties(){
+        PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer = new PropertySourcesPlaceholderConfigurer();
+        return propertySourcesPlaceholderConfigurer;
+    }
+
+}
